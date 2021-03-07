4d7ace1669a8d97b343eb523e4c2e7b110348950
00bayz <zhuoyu.yang@ndus.edu>
2021-03-07 09:15:35 -0600

    [Dev] Set up main layout (MainLayout) that divides the game UI into two columns; the left column is dynamic (percent-based) and the right column i static at a width of 240px

--- MarbleGame/Form1.Designer.cs
+++ MarbleGame/Form1.Designer.cs
@@ -29,14 +29,30 @@ namespace MarbleGame
         /// </summary>
         private void InitializeComponent()
         {
+            this.MainLayout = new System.Windows.Forms.TableLayoutPanel();
             this.SuspendLayout();
+            // 
+            // MainLayout
+            // 
+            this.MainLayout.ColumnCount = 2;
+            this.MainLayout.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.MainLayout.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 240F));
+            this.MainLayout.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.MainLayout.Location = new System.Drawing.Point(0, 0);
+            this.MainLayout.Margin = new System.Windows.Forms.Padding(0);
+            this.MainLayout.Name = "MainLayout";
+            this.MainLayout.RowCount = 1;
+            this.MainLayout.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.MainLayout.Size = new System.Drawing.Size(840, 500);
+            this.MainLayout.TabIndex = 0;
             // 
             // Game
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(50)))), ((int)(((byte)(50)))), ((int)(((byte)(50)))));
-            this.ClientSize = new System.Drawing.Size(800, 500);
+            this.ClientSize = new System.Drawing.Size(840, 500);
+            this.Controls.Add(this.MainLayout);
             this.Font = new System.Drawing.Font("Liberation Mono", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.Name = "Game";
             this.Text = "Marble Game";
@@ -45,6 +61,8 @@ namespace MarbleGame
         }
 
         #endregion
+
+        private System.Windows.Forms.TableLayoutPanel MainLayout;
     }
 }
 
