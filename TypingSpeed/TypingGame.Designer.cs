8f8b096259e5870d0c8a108b0dd5e4eb99657955
00bayz <zhuoyu.yang@ndus.edu>
2021-04-07 14:05:37 -0500

    [Dev] Assignment 6 - Complete third project required of assignment 6. The third project is essentially a simple game that tests a user's typing prowess by timing how long it takes them to correct type a random string of letters; there are three levels of difficulty to choose from.

--- TypingSpeed/TypingGame.Designer.cs
+++ TypingSpeed/TypingGame.Designer.cs
@@ -29,22 +29,148 @@ namespace TypingSpeed
         /// </summary>
         private void InitializeComponent()
         {
+            this.clock1 = new CustomDLL.Clock();
+            this.startBtn = new System.Windows.Forms.Button();
+            this.resTextBox = new System.Windows.Forms.TextBox();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.pauseBtn = new System.Windows.Forms.Button();
+            this.easyRad = new System.Windows.Forms.RadioButton();
+            this.mediumRad = new System.Windows.Forms.RadioButton();
+            this.hardRad = new System.Windows.Forms.RadioButton();
+            this.diffLabel = new System.Windows.Forms.Label();
             this.SuspendLayout();
+            // 
+            // clock1
+            // 
+            this.clock1.BackColor = System.Drawing.SystemColors.Control;
+            this.clock1.ElapsedMins = 0;
+            this.clock1.ElapsedSecs = 0;
+            this.clock1.Location = new System.Drawing.Point(12, 428);
+            this.clock1.Margin = new System.Windows.Forms.Padding(5, 4, 5, 4);
+            this.clock1.Name = "clock1";
+            this.clock1.Size = new System.Drawing.Size(460, 160);
+            this.clock1.TabIndex = 0;
+            // 
+            // startBtn
+            // 
+            this.startBtn.Location = new System.Drawing.Point(12, 220);
+            this.startBtn.Name = "startBtn";
+            this.startBtn.Size = new System.Drawing.Size(460, 50);
+            this.startBtn.TabIndex = 1;
+            this.startBtn.Text = "START";
+            this.startBtn.UseVisualStyleBackColor = true;
+            this.startBtn.Click += new System.EventHandler(this.startBtn_Click);
+            // 
+            // resTextBox
+            // 
+            this.resTextBox.Location = new System.Drawing.Point(12, 360);
+            this.resTextBox.Name = "resTextBox";
+            this.resTextBox.Size = new System.Drawing.Size(460, 26);
+            this.resTextBox.TabIndex = 2;
+            this.resTextBox.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.resTextBox_KeyPress);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Enabled = false;
+            this.richTextBox1.Font = new System.Drawing.Font("Liberation Mono", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.richTextBox1.Location = new System.Drawing.Point(12, 12);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(460, 159);
+            this.richTextBox1.TabIndex = 3;
+            this.richTextBox1.Text = "Instructions:\n\n1) Click \"Start\" to generate random string of characters\n\n2) Type " +
+    "characters in text field (NOT case-sensitive)\n\n3) Press \"Enter\" when done\n\n4) Se" +
+    "e how fast you type!";
+            // 
+            // pauseBtn
+            // 
+            this.pauseBtn.Location = new System.Drawing.Point(12, 280);
+            this.pauseBtn.Name = "pauseBtn";
+            this.pauseBtn.Size = new System.Drawing.Size(460, 50);
+            this.pauseBtn.TabIndex = 4;
+            this.pauseBtn.Text = "PAUSE";
+            this.pauseBtn.UseVisualStyleBackColor = true;
+            this.pauseBtn.Visible = false;
+            this.pauseBtn.Click += new System.EventHandler(this.pauseBtn_Click);
+            // 
+            // easyRad
+            // 
+            this.easyRad.AutoSize = true;
+            this.easyRad.ForeColor = System.Drawing.SystemColors.ControlLight;
+            this.easyRad.Location = new System.Drawing.Point(242, 192);
+            this.easyRad.Name = "easyRad";
+            this.easyRad.Size = new System.Drawing.Size(66, 22);
+            this.easyRad.TabIndex = 5;
+            this.easyRad.Text = "Easy";
+            this.easyRad.UseVisualStyleBackColor = true;
+            // 
+            // mediumRad
+            // 
+            this.mediumRad.AutoSize = true;
+            this.mediumRad.Checked = true;
+            this.mediumRad.ForeColor = System.Drawing.SystemColors.ControlLight;
+            this.mediumRad.Location = new System.Drawing.Point(314, 192);
+            this.mediumRad.Name = "mediumRad";
+            this.mediumRad.Size = new System.Drawing.Size(86, 22);
+            this.mediumRad.TabIndex = 6;
+            this.mediumRad.TabStop = true;
+            this.mediumRad.Text = "Medium";
+            this.mediumRad.UseVisualStyleBackColor = true;
+            // 
+            // hardRad
+            // 
+            this.hardRad.AutoSize = true;
+            this.hardRad.ForeColor = System.Drawing.SystemColors.ControlLight;
+            this.hardRad.Location = new System.Drawing.Point(406, 192);
+            this.hardRad.Name = "hardRad";
+            this.hardRad.Size = new System.Drawing.Size(66, 22);
+            this.hardRad.TabIndex = 7;
+            this.hardRad.Text = "Hard";
+            this.hardRad.UseVisualStyleBackColor = true;
+            // 
+            // diffLabel
+            // 
+            this.diffLabel.AutoSize = true;
+            this.diffLabel.ForeColor = System.Drawing.SystemColors.ControlLight;
+            this.diffLabel.Location = new System.Drawing.Point(12, 194);
+            this.diffLabel.Name = "diffLabel";
+            this.diffLabel.Size = new System.Drawing.Size(198, 18);
+            this.diffLabel.TabIndex = 8;
+            this.diffLabel.Text = "Select Difficulty: ";
             // 
             // TypingGame
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 18F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.ControlDarkDark;
-            this.ClientSize = new System.Drawing.Size(464, 441);
+            this.ClientSize = new System.Drawing.Size(484, 601);
+            this.Controls.Add(this.diffLabel);
+            this.Controls.Add(this.hardRad);
+            this.Controls.Add(this.mediumRad);
+            this.Controls.Add(this.easyRad);
+            this.Controls.Add(this.pauseBtn);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.resTextBox);
+            this.Controls.Add(this.startBtn);
+            this.Controls.Add(this.clock1);
             this.Font = new System.Drawing.Font("Liberation Mono", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.Margin = new System.Windows.Forms.Padding(5, 4, 5, 4);
             this.Name = "TypingGame";
             this.Text = "Typing Game";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private CustomDLL.Clock clock1;
+        private System.Windows.Forms.Button startBtn;
+        private System.Windows.Forms.TextBox resTextBox;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button pauseBtn;
+        private System.Windows.Forms.RadioButton easyRad;
+        private System.Windows.Forms.RadioButton mediumRad;
+        private System.Windows.Forms.RadioButton hardRad;
+        private System.Windows.Forms.Label diffLabel;
     }
 }
--- TypingSpeed/TypingGame.cs
+++ TypingSpeed/TypingGame.cs
@@ -12,11 +12,89 @@ namespace TypingSpeed
 {
     public partial class TypingGame : Form
     {
+        bool isGameRunning = false;
+
         public TypingGame()
         {
             InitializeComponent();
         }
 
+        private int stringLength()
+        {
+            if (easyRad.Checked)
+            {
+                return 5;
+            }
+            else if (hardRad.Checked)
+            {
+                return 15;
+            }
+            else
+            {
+                return 10;
+            }
+        }
 
+        private string generateRandomString()
+        {
+            string randomString = "";
+            char[] characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".ToCharArray();
+            Random randIndex = new Random();
+            for (int i = 0; i < stringLength(); i++)
+            {
+                randomString += characters[randIndex.Next(0, 25)].ToString();
+            }
+            return randomString;
+        }
+
+        private void startBtn_Click(object sender, EventArgs e)
+        {
+            if (!isGameRunning)
+            {
+                clock1.Reset();
+                isGameRunning = true;
+                clock1.Start();
+                pauseBtn.Visible = true;
+                startBtn.Text = generateRandomString();
+                startBtn.Enabled = false;
+                resTextBox.Focus();
+            }
+        }
+
+        private void resTextBox_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar == (char)Keys.Enter && isGameRunning)
+            {
+                string res = resTextBox.Text.ToUpper();
+                if (res == startBtn.Text)
+                {
+                    clock1.Stop();
+                    pauseBtn.Visible = false;
+                    startBtn.Text = $"Time taken: {clock1.ElapsedMins}:{clock1.ElapsedSecs}. Click to try again.";
+                    isGameRunning = false;
+                    resTextBox.Text = "";
+                    startBtn.Enabled = true;
+                }
+            }
+        }
+
+        private void pauseBtn_Click(object sender, EventArgs e)
+        {
+            if (pauseBtn.Text == "PAUSE")
+            {
+                clock1.Pause();
+                startBtn.Visible = false;
+                resTextBox.Enabled = false;
+                pauseBtn.Text = "RESUME";
+            }
+            else if (pauseBtn.Text == "RESUME")
+            {
+                clock1.Resume();
+                startBtn.Visible = true;
+                resTextBox.Enabled = true;
+                pauseBtn.Text = "PAUSE";
+                resTextBox.Focus();
+            }
+        }
     }
 }
